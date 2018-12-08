050e94ca60483918d38ea5696bc60b3a0054f656
Jaygem <youness.kafia@devinci.fr>
2018-12-08 11:04:48 +0100

    First Commit

---
+++ test.py
@@ -0,0 +1,25 @@
+import os
+import json
+import numpy as np
+import pandas as pd
+from pandas.io.json import json_normalize
+
+def load_df(csv_path='./Dataset/train.csv', nrows=None):
+    JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
+    
+    df = pd.read_csv(csv_path, 
+                     converters={column: json.loads for column in JSON_COLUMNS}, 
+                     dtype={'fullVisitorId': 'str'}, # Important!!
+                     nrows=nrows)
+    
+    for column in JSON_COLUMNS:
+        column_as_df = json_normalize(df[column])
+        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
+        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
+    print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")
+    return df
+
+
+data1 = load_df(nrows=20000)
+
+print(data1.head())
